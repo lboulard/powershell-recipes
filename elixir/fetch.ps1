d9f1f41f95308e0f0dc2a74e774be482c4ad0c22
Laurent Boulard <laurent.boulard@gmail.com>
2024-05-30 14:11:26 +0200

    Elixir uses GitHub API to discover files to download
    Can see releases for Erlang 27 when ready

--- elixir/fetch.ps1
+++ elixir/fetch.ps1
@@ -1,45 +1,150 @@
 $ErrorActionPreference = "Stop"
 
-$versionPattern = "/(?<release>v(?<version>\d+\.\d+\.\d+))$"
-$project = "elixir-lang/elixir"
+$versionPattern = "/(?<tag>v(?<version>\d+\.\d+(\.\d+){0,2}))$"
+$project = "chrisant996/dirx"
 
-$feedURL = "https://github.com/$project/releases.atom"
-
-$atomFeed = Invoke-RestMethod -Uri $feedURL
-
-$lastVersionURL = $atomFeed.link.href | Where-Object {
-  $_ -match $versionPattern
-} | Sort-Object -Descending -Property {
-  if ($_ -match $versionPattern) {
-    $Matches.version -as [version]
+$lastReleaseURL = "https://github.com/$project/releases/latest"
+try {
+  $response = Invoke-WebRequest -Method Head -Uri $lastReleaseURL -ErrorAction Ignore
+  $statusCode = $response.StatusCode
+  Write-Verbose ("`$statusCode={0}" -f $statusCode)
+} catch {
+  Write-Verbose $_.Exception
+  $statusCode = $_.Exception.Response.StatusCode.value__
+}
+if ($statusCode -eq 302) {
+  $lastVersionURL = $response.Headers.Location
+} elseif ($statusCode -eq 200) {
+  if ($response.BaseResponse.ResponseUri -ne $null) {
+    # PS5.1
+    $lastVersionURL = $response.BaseResponse.ResponseUri.AbsoluteUri
+    Write-Verbose ("`$lastVersionURL={0}" -f $lastVersionURL)
+  } elseif ($response.BaseResponse.RequestMessage.RequestUri -ne $null) {
+    # PS7
+    $lastVersionURL = $response.BaseResponse.RequestMessage.RequestUri.AbsoluteUri
+    Write-Verbose ("`$lastVersionURL={0}" -f $lastVersionURL)
   }
-},{ $_ }
+} else {
+  Write-Error ("HTTP Response {0}: {1}" -f $statusCode, $response.StatusDescription)
+  throw ("unexpected response {0} for {1}" -f $statusCode, $lastReleaseURL)
+}
 
 if ($lastVersionURL) {
   Write-Host "# last Version" $lastVersionURL -Separator "`n"
 } else {
-  Write-Host "# last Version" $atomFeed.link.href -Separator "`n"
-  throw "no release found at $feedURL"
+  throw "no release found at ${lastReleaseURL}"
 }
 
-($lastVersionURL[0] -match $versionPattern) | Out-Null
-$release = $Matches.release
+($lastVersionURL -match $versionPattern) | Out-Null
+$tag = $Matches.tag
 $version = $Matches.version
 
-$repo = "https://github.com/$project/releases/download/$release"
+Add-Type -TypeDefinition @"
+using System;
+using System.Runtime.InteropServices;
+using System.Text;
 
-$files =,"elixir-otp-26.exe#elixir-otp-26-$version.exe"
-$files += "elixir-otp-26.exe.sha256sum#elixir-otp-26-$version.exe.sha256sum"
-$files += "Docs.zip#Docs-$version.zip"
-$files += "Docs.zip.sha256sum#Docs-$version.zip.sha256sum"
+public class CredentialManager {
+    [DllImport("advapi32", SetLastError = true, CharSet = CharSet.Unicode)]
+    public static extern bool CredRead(string target, int type, int reservedFlag, out IntPtr credentialPtr);
+
+    [DllImport("advapi32", SetLastError = true)]
+    public static extern void CredFree(IntPtr cred);
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+    public struct CREDENTIAL {
+        public int Flags;
+        public int Type;
+        public IntPtr TargetName;
+        public IntPtr Comment;
+        public long LastWritten;
+        public int CredentialBlobSize;
+        public IntPtr CredentialBlob;
+        public int Persist;
+        public int AttributeCount;
+        public IntPtr Attributes;
+        public IntPtr TargetAlias;
+        public IntPtr UserName;
+    }
+
+    public static string GetCredential(string targetName) {
+        IntPtr credPtr;
+        if (CredRead(targetName, 1, 0, out credPtr)) {
+            var credential = (CREDENTIAL)Marshal.PtrToStructure(credPtr, typeof(CREDENTIAL));
+            string password = Marshal.PtrToStringUni(credential.CredentialBlob, credential.CredentialBlobSize / 2);
+            CredFree(credPtr);
+            return password;
+        } else {
+            throw new Exception("Failed to retrieve credential from Windows Credential Manager.");
+        }
+    }
+}
+"@
+
+# check API key exists with 'cmdkey.exe /list:git:https://github.com'
+
+function Get-GitHubToken {
+  param (
+    [string]$targetName = "git:https://github.com"
+  )
+
+  try {
+    $token = [CredentialManager]::GetCredential($targetName)
+    return $token
+  } catch {
+    Write-Error $_.Exception.Message
+    return $null
+  }
+}
+
+# filenames are not enough to find release files
+# use GitHub API
+
+$headers = @{
+  'Accept'               = 'application/vnd.github+json'
+  "X-GitHub-Api-Version" = "2022-11-28"
+}
+
+$githubToken = $env:GITHUB_TOKEN
+if (-not $githubToken) {
+  # Get the GitHub API token from Windows Credential for git
+  $githubToken = Get-GitHubToken
+  if ($githubToken) {
+    $headers['Authorization'] = 'Bearer ' + $githubToken
+  }
+} else {
+  $headers['Authorization'] = 'token ' + $githubToken
+}
+
+$wanted = "elixir-otp-(?:2[567])\.exe"
+$wanted = "(${wanted})|(Docs\.zip)"
+$wanted = "^(${wanted})(\.sha256sum)?$"
+
+try {
+  $json = Invoke-WebRequest -Uri "https://api.github.com/repos/$project/releases/tags/$tag" -Headers $headers -UseBasicParsing
+  $release = $json.Content | ConvertFrom-Json
+  $files = $release.assets | ForEach-Object {
+    $asset = $_
+    $name = $asset.name
+    if ($name -match $wanted) {
+      $name = $name -replace "\.(exe|zip)", "-${version}`$0"
+      "$($asset.browser_download_url)#${name}"
+    }
+  }
+} catch {
+  Write-Error "Error: $($_.Exception.Message), line $($_.InvocationInfo.ScriptLineNumber)"
+  break
+}
+
+$headers['Accept'] = 'application/octet-stream'
 
 $files | ForEach-Object {
-  $parts = $_.Split('#',2)
-  $src = "$repo/" + $parts[0]
-  if ($parts.Length -eq 2) {
-    $dest = $parts[1]
+  $url = [System.Uri]($_)
+  $src = $url.AbsoluteUri
+  if ($url.Fragment -and ($url.Fragment.Length -gt 1)) {
+    $dest = [Uri]::UnescapeDataString($url.Fragment.Substring(1))
   } else {
-    $dest = $parts[0]
+    $dest = [Uri]::UnescapeDataString($url.Segments[-1])
   }
 
   Write-Host "# $dest"
@@ -47,7 +152,7 @@ $files | ForEach-Object {
     try {
       Write-Host "  -> $src"
       $tmpFile = "$dest.tmp"
-      Invoke-WebRequest -Uri "$src" -OutFile $tmpFile -UseBasicParsing
+      Invoke-WebRequest -Uri "$src" -OutFile $tmpFile -Headers $headers -UseBasicParsing
       Move-Item -Path $tmpFile -Destination "$dest"
     } catch {
       Write-Error "Error: $($_.Exception.Message), line $($_.InvocationInfo.ScriptLineNumber)"
