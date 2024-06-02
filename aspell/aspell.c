b7701067b793a6167d85d3b7a67ab4945fd45783
Laurent Boulard <laurent.boulard@gmail.com>
2024-06-02 16:52:23 +0200

    Update aspell bridge to latest version

--- aspell/aspell.c
+++ aspell/aspell.c
@@ -1,12 +1,9 @@
-#include <Windows.h>
 #include <Shlwapi.h>
-#include <io.h>
+#include <Windows.h>
 #include <fcntl.h>
-#include <tchar.h>
+#include <io.h>
 #include <stdio.h>
-
-#define PROGRAM_FILES TEXT("C:\\Program Files")
-#define ASPELL_PATH   TEXT("Aspell\\bin\\aspell.exe")
+#include <tchar.h>
 
 static HANDLE
 duplicate(HANDLE handle)
@@ -17,20 +14,65 @@ duplicate(HANDLE handle)
         return handle;
 
     if (DuplicateHandle(
-            GetCurrentProcess(),    /* Source process handle */
-            handle,                 /* Source handle to duplicate */
-            GetCurrentProcess(),    /* Target process handle */
-            &duplicatedHandle,      /* Pointer to the duplicated handle */
-            0,                      /* Desired access rights (0 for same access) */
-            TRUE,                   /* Inheritable flag (FALSE for non-inheritable) */
-            DUPLICATE_SAME_ACCESS   /* Options */
+            GetCurrentProcess(), /* Source process handle */
+            handle,              /* Source handle to duplicate */
+            GetCurrentProcess(), /* Target process handle */
+            &duplicatedHandle,   /* Pointer to the duplicated handle */
+            0,                   /* Desired access rights (0 for same access) */
+            TRUE, /* Inheritable flag (FALSE for non-inheritable) */
+            DUPLICATE_SAME_ACCESS /* Options */
                        ))
         return duplicatedHandle;
     else {
-        fprintf(stderr, "Error creating duplicate handle. Error code: %lu\n", GetLastError());
+        fprintf(stderr, "Error creating duplicate handle. Error code: %lu\n",
+                GetLastError());
         exit(1);
     }
     return duplicatedHandle;
+}
+
+static const TCHAR *
+aspellInstallPathFromRegistry()
+{
+    static const TCHAR keyPath[] = TEXT("SOFTWARE\\Aspell");
+    DWORD              dwType    = REG_SZ;
+    HKEY               hKey;
+    LPTSTR             path;
+    DWORD              length;
+
+    LSTATUS result;
+
+    result =
+        RegOpenKeyEx(HKEY_LOCAL_MACHINE, keyPath, 0, KEY_QUERY_VALUE, &hKey);
+    if (result != ERROR_SUCCESS) {
+        fprintf(stderr, "RegOpenKeyEx failed: %ld\n", GetLastError());
+        return NULL;
+    }
+
+    result = RegQueryValueEx(hKey, NULL, NULL, &dwType, NULL, &length);
+    if (result == ERROR_SUCCESS) {
+        path = (LPTSTR)LocalAlloc(0, length * sizeof(path[0]));
+        if (path == NULL) {
+            fprintf(stderr, "LocalAlloc failed: %ld\n", GetLastError());
+            RegCloseKey(hKey);
+            return NULL;
+        }
+
+        result =
+            RegQueryValueEx(hKey, NULL, NULL, &dwType, (LPBYTE)path, &length);
+        if (result != ERROR_SUCCESS) {
+            printf("RegQueryValueEx failed: %ld\n", GetLastError());
+            RegCloseKey(hKey);
+            return NULL;
+        }
+    } else if (result == ERROR_MORE_DATA)
+        fprintf(stderr, "Buffer too small\n");
+    else
+        fprintf(stderr, "RegQueryValueEx failed: %ld\n", GetLastError());
+
+    RegCloseKey(hKey);
+
+    return path;
 }
 
 int
@@ -38,22 +80,13 @@ main()
 {
     _setmode(_fileno(stdout), _O_WTEXT);
 
-    TCHAR programFiles[MAX_PATH];
-    DWORD programFilesSize = GetEnvironmentVariable(TEXT("ProgramFiles(x86)"),
-                                                    programFiles,
-                                                    MAX_PATH);
-    if (programFilesSize == 0)
-        programFilesSize = GetEnvironmentVariable(TEXT("ProgramFiles"),
-                                                  programFiles,
-                                                  MAX_PATH);
-
-    if (programFilesSize == 0)
-        memcpy(programFiles, PROGRAM_FILES, sizeof(PROGRAM_FILES) / sizeof(programFiles[0]));
+    const TCHAR *aspellInstallPath = aspellInstallPathFromRegistry();
+    if (!aspellInstallPath)
+        return 1;
 
     TCHAR userProfile[MAX_PATH];
-    DWORD userProfileSize = GetEnvironmentVariable(TEXT("USERPROFILE"),
-                                                   userProfile,
-                                                   MAX_PATH);
+    DWORD userProfileSize =
+        GetEnvironmentVariable(TEXT("USERPROFILE"), userProfile, MAX_PATH);
 
     if (userProfileSize == 0) {
         fprintf(stderr, "Error reading USERPROFILE environment variable.\n");
@@ -71,8 +104,9 @@ main()
     }
 
     TCHAR aspellExecutablePath[MAX_PATH];
-    if (!PathCombine(aspellExecutablePath, programFiles, ASPELL_PATH))
-        fprintf(stderr, "error constructin aspell.exe executable path.\n");
+    if (!PathCombine(aspellExecutablePath, aspellInstallPath,
+                     TEXT("bin\\aspell.exe")))
+        fprintf(stderr, "error constructing aspell.exe executable path.\n");
 
     /* get arguments after program name */
     LPTSTR commandLine = GetCommandLine();
@@ -94,29 +128,31 @@ main()
 
     ZeroMemory(&pi, sizeof(PROCESS_INFORMATION));
 
-    if (!CreateProcess(
-            aspellExecutablePath,
-            commandLine,
-            NULL,   /* Process handle not inheritable */
-            NULL,   /* Thread handle not inheritable */
-            TRUE,   /* Set handle inheritance to TRUE */
-            0,      /* No creation flags */
-            NULL,   /* Use parent's environment block */
-            NULL,   /* Use parent's starting directory */
-            &si,    /* Pointer to STARTUPINFO structure */
-            &pi     /* Pointer to PROCESS_INFORMATION structure */
+    if (!CreateProcess(aspellExecutablePath, commandLine,
+                       NULL, /* Process handle not inheritable */
+                       NULL, /* Thread handle not inheritable */
+                       TRUE, /* Set handle inheritance to TRUE */
+                       0,    /* No creation flags */
+                       NULL, /* Use parent's environment block */
+                       NULL, /* Use parent's starting directory */
+                       &si,  /* Pointer to STARTUPINFO structure */
+                       &pi   /* Pointer to PROCESS_INFORMATION structure */
                       ))
     {
-        fprintf(stderr, "Error creating subprocess. Error code: %ld\n", GetLastError());
+        fprintf(stderr, "Error creating subprocess. Error code: %ld\n",
+                GetLastError());
         return 1;
     }
 
     /* Wait for the subprocess to exit */
     WaitForSingleObject(pi.hProcess, INFINITE);
 
+    DWORD exitCode;
+    GetExitCodeProcess(pi.hProcess, &exitCode);
+
     /* Close process and thread handles */
     CloseHandle(pi.hProcess);
     CloseHandle(pi.hThread);
 
-    return 0;
+    return exitCode;
 }
