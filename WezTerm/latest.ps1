f99b2086fe8e59c56fd5067b21616a43672f9005
Laurent Boulard <laurent.boulard@gmail.com>
2024-06-02 15:10:32 +0200

    WezTerm: download using only $project/releases/latest entry point

--- WezTerm/latest.ps1
+++ WezTerm/latest.ps1
@@ -1,44 +1,61 @@
 $ErrorActionPreference = "Stop"
 
 # Example: .../tag/20240203-110809-5046fc22
-$versionPattern = '/(?<version>\d{8}-\d+-[0-9a-f]+)$'
+$versionPattern = '/(?<tag>(?<version>\d{8}-\d+-[0-9a-f]+))$'
 $project = "wez/wezterm"
 
-$feedURL = "https://github.com/$project/releases.atom"
-
-$atomFeed = Invoke-RestMethod -Uri $feedURL
-
-$lastVersionURL = $atomFeed.link.href | Where-Object {
-  $_ -match $versionPattern
-} | Sort-Object -Descending -Property {
-  $_ -match $versionPattern | Out-Null
-  $Matches.version -split "-"
+$lastReleaseURL = "https://github.com/$project/releases/latest"
+try {
+  $response = Invoke-WebRequest -Method Head -Uri $lastReleaseURL -ErrorAction Ignore
+  $statusCode = $response.StatusCode
+  Write-Verbose ("`$statusCode={0}" -f $statusCode)
+} catch {
+  Write-Verbose $_.Exception
+  $statusCode = $_.Exception.Response.StatusCode.value__
+}
+if ($statusCode -eq 302) {
+  $lastVersionURL = $response.Headers.Location
+} elseif ($statusCode -eq 200) {
+  if ($response.BaseResponse.ResponseUri -ne $null) {
+    # PS5.1
+    $lastVersionURL = $response.BaseResponse.ResponseUri.AbsoluteUri
+    Write-Verbose ("`$lastVersionURL={0}" -f $lastVersionURL)
+  } elseif ($response.BaseResponse.RequestMessage.RequestUri -ne $null) {
+    # PS7
+    $lastVersionURL = $response.BaseResponse.RequestMessage.RequestUri.AbsoluteUri
+    Write-Verbose ("`$lastVersionURL={0}" -f $lastVersionURL)
+  }
+} else {
+  Write-Error ("HTTP Response {0}: {1}" -f $statusCode, $response.StatusDescription)
+  throw ("unexpected response {0} for {1}" -f $statusCode, $lastReleaseURL)
 }
 
 if ($lastVersionURL) {
   Write-Host "# last Version" $lastVersionURL -Separator "`n"
 } else {
-  Write-Host "# last Version" $atomFeed.link.href -Separator "`n"
-  throw "no release found at $feedURL"
+  throw "no release found at ${lastReleaseURL}"
 }
-($lastVersionURL[0] -match $versionPattern) | Out-Null
-$release = $Matches.version
+
+($lastVersionURL -match $versionPattern) | Out-Null
+$tag = $Matches.tag
 $version = $Matches.version
 
-$repo = "https://github.com/$project/releases/download/$release"
+$repo = "https://github.com/$project/releases/download/$tag"
 
-$files =,"WezTerm-windows-$version.zip"
-$files += "WezTerm-windows-$version.zip.sha256"
-$files += "WezTerm-$version-setup.exe"
-$files += "WezTerm-$version-setup.exe.sha256"
+$files = @(
+  "WezTerm-windows-$version.zip"
+  "WezTerm-windows-$version.zip.sha256"
+  "WezTerm-$version-setup.exe"
+  "WezTerm-$version-setup.exe.sha256"
+) | ForEach-Object { "$repo/$_" }
 
 $files | ForEach-Object {
-  $parts = $_.Split('#',2)
-  $src = "$repo/" + $parts[0]
-  if ($parts.Length -eq 2) {
-    $dest = $parts[1]
+  $url = [System.Uri]($_)
+  $src = $url.AbsoluteUri
+  if ($url.Fragment -and ($url.Fragment.Length -gt 1)) {
+    $dest = [Uri]::UnescapeDataString($url.Fragment.TrimStart('#'))
   } else {
-    $dest = $parts[0]
+    $dest = [Uri]::UnescapeDataString($url.Segments[-1])
   }
 
   Write-Host "# $dest"
