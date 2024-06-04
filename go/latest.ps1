b6f8547a6f82b6ae1a6bbbe3cba0848d1e67729e
Laurent Boulard <laurent.boulard@gmail.com>
2024-06-04 22:14:43 +0200

    Downloader for Go language set date to last modified and create folder on demand

--- go/latest.ps1
+++ go/latest.ps1
@@ -56,7 +56,6 @@ $maintained = $releases | ForEach-Object {
 # last version is downloaded in current folder
 # other version are downloaded in "$version" folder
 
-$folders = @()
 $files = $maintained | ForEach-Object -Begin { $last = $null } {
   $dl = New-Object System.Uri -ArgumentList $url,$_.href
   if ((-not $last) -or ($last -eq $_.version)) {
@@ -65,37 +64,45 @@ $files = $maintained | ForEach-Object -Begin { $last = $null } {
   } else {
     $branch = "$($_.Major).$($_.Minor)"
     "$dl#$branch/$($_.Release)"
-    $folders += $branch
-  }
-}
-
-# Create folders as needed
-$folders | Sort-Object -Uniq | ForEach-Object {
-  $folder = $_
-  if (-not (Test-Path $folder -PathType Container)) {
-    Write-Host "# New folder $folder"
-    New-Item -ItemType Directory -Path "$folder" -Force | Out-Null
   }
 }
 
 # and download all
 
+$folders = @{}  # remember created folder to create only once
+
 $files | ForEach-Object {
-  $parts = $_.Split('#',2)
-  $src = $parts[0]
-  if ($parts.Length -eq 2) {
-    $dest = $parts[1]
+  $url = [System.Uri]($_)
+  $src = $url.AbsoluteUri
+  if ($url.Fragment -and ($url.Fragment.Length -gt 1)) {
+    $dest = [Uri]::UnescapeDataString($url.Fragment.Substring(1))
   } else {
-    $dest = $parts[0]
+    $dest = [Uri]::UnescapeDataString($url.Segments[-1])
   }
 
   Write-Host "# $dest"
-
   if (-not (Test-Path $dest)) {
     try {
       Write-Host "  -> $src"
+      $parent = Split-Path -Parent -Path $dest
+      if ($parent -and -not $folders.Contains($parent)) {
+        if (-not (Test-Path $parent -PathType Container)) {
+          New-Item -Path $parent -ItemType Container | Out-Null
+        }
+        $folders.Add($parent, $True)
+      }
       $tmpFile = "$dest.tmp"
-      Invoke-WebRequest -Uri "$src" -OutFile $tmpFile -UseBasicParsing
+      $result = Invoke-WebRequest -Uri "$src" -OutFile $tmpFile -UseBasicParsing -PassThru
+      $lastModified = $result.Headers['Last-Modified']
+      if ($lastModified) {
+        try {
+          $lastModifiedDate = Get-Date $lastModified[0]
+          (Get-Item $tmpFile).LastWriteTimeUtc = $lastModifiedDate
+        } catch {
+          Write-Error "Error: $($_.Exception.Message)"
+          Write-Error "Date: $lastModified"
+        }
+      }
       Move-Item -Path $tmpFile -Destination "$dest"
     } catch {
       Write-Error "Error: $($_.Exception.Message), line $($_.InvocationInfo.ScriptLineNumber)"
