33df1f99e02000335a1b70d2b68b1b0e86c1117f
Laurent Boulard <laurent.boulard@gmail.com>
2024-06-13 20:12:58 +0200

    Script to download Windows ConEmu Terminal emulator

---
+++ ConEmu/latest.ps1
@@ -0,0 +1,44 @@
+$ErrorActionPreference = "Stop"
+
+# https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_preference_variables?view=powershell-7.4#verbosepreference
+# $VerbosePreference = "Continue"
+
+$project = "Maximus5/ConEmu"
+$tagPattern = "/(?<tag>v(?<version>\d+\.\d+(\.\d+){0,2}))$"
+$wanted = "^ConEmu(Pack|Setup)\.\d+\.(7z|exe)$"
+
+Import-Module lboulard-Recipes
+
+$lastVersionUrl = Get-GitHubLatestReleaseUrl $project
+
+if ($lastVersionURL) {
+  Write-Host "# last Version" $lastVersionURL -Separator "`n"
+} else {
+  throw "no release found for ${project}"
+}
+
+if ($lastVersionURL -match $tagPattern) {
+  $tag = $Matches.tag
+  $version = $Matches.version
+} else {
+  throw "no tag match at ${lastVersionUrl}"
+}
+
+# filenames are not enough to find release files
+# use GitHub API
+
+$githubToken = Get-GitHubToken
+
+$files = Find-GitHubAssets $project $tag $version -AssetPattern $wanted -Token $githubToken
+
+$headers = @{
+  'Accept'               = 'application/vnd.github+json'
+  "X-GitHub-Api-Version" = "2022-11-28"
+}
+if ($githubToken) {
+  $headers['Authorization'] = 'token ' + $githubToken
+}
+
+if ($files) {
+  Get-Url $files -Headers $headers
+}
