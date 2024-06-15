051bb4b27c1d1b715604068760e972247579bbd8
Laurent Boulard <laurent.boulard@gmail.com>
2024-06-15 17:49:06 +0200

    Scripts for download of Gitea server and tea command line tool

---
+++ gitea/latest.ps1
@@ -0,0 +1,70 @@
+# Find latest release of Gitea installer for a branch or latest release version
+
+param(
+  [string]$Branch = ""
+)
+
+# https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_preference_variables?view=powershell-7.4#verbosepreference
+# $VerbosePreference = "Continue"
+# $DebugPreference = "Continue"
+
+$ErrorActionPreference = "Stop"
+
+$IndexURL = "https://dl.gitea.com/gitea/"
+if ($Branch) {
+  $branchRegex = "(?<version>" + [regex]::escape($Branch) + "\.\d+)/"
+} else {
+  $branchRegex = "(?<version>\d+\.\d+\.\d+)/"
+}
+$versionPattern = "(?<version>\d+\.\d+\.\d+)"
+
+$wanted = "/gitea-" + $versionPattern + "-windows-4\.0-amd64\.exe(\.(asc|sha256|asc\.sha256))?$"
+
+$UserAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+
+$url = [System.Uri]$IndexURL
+
+Write-Host "Reading $IndexURL"
+$response = Invoke-WebRequest -Uri $IndexURL -UseBasicParsing -UserAgent $UserAgent
+
+$files = $response.links.href | ForEach-Object {
+  if ($_ -match $branchRegex) {
+    @{target = $_; version = [version]$Matches.version }
+  }
+} | Sort-Object -Descending -Property {
+  $_.version
+}, { $_ } | % { $_.target } | ForEach-Object {
+  New-Object System.Uri -ArgumentList $url, $_
+} | ForEach-Object -Begin { $found = $False } -Process {
+  if (-not $found) {
+    Write-Host "Reading $_"
+    $folderURL = $_
+    $folder = Invoke-WebRequest -Uri $_ -UseBasicParsing -UserAgent $UserAgent
+    $folder.links.href | Where-Object {
+      $_ -match $wanted
+    } | ForEach-Object {
+      $found = $True
+      Write-Debug "accepted: ${folderURL} : $_"
+      (New-Object System.Uri -ArgumentList $folderURL, $_).AbsoluteUri
+    }
+  }
+}
+
+if ($files) {
+  $files[0] -match $versionPattern | Out-Null
+  $version = $Matches.version -as [version]
+} elseif ($Branch) {
+  throw "no release found for ${Branch}"
+} else {
+  throw "no release found"
+}
+
+Write-Host "# last Version $version"
+
+$files = $files | % { 
+  $u = [System.Uri]$_
+  $name = $u.Segments[-1]
+  "$_#gitea-${version}/${name}"
+}
+
+Get-Url $files
