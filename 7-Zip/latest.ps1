43abf53ea0d253e14ec32f413f0c7d55c01c3f9c
Laurent Boulard <laurent.boulard@gmail.com>
2024-06-20 11:36:40 +0200

    Script to download latest release of 7-Zip archive manager tool

---
+++ 7-Zip/latest.ps1
@@ -0,0 +1,22 @@
+$ErrorActionPreference = "Stop"
+
+# https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_preference_variables?view=powershell-7.4#verbosepreference
+# $VerbosePreference = "Continue"
+
+$project = "ip7z/7zip"
+$tagPattern = "(?<version>\d+\.\d+(\.\d+)?)"
+
+Import-Module lboulard-Recipes
+
+Get-GitHubAssetsOfLatestRelease $project $tagPattern -FileSelection {
+  $release = $version -replace '\.', ''
+  @(
+    "7z${release}-x64.exe"
+    "7z${release}-x64.msi"
+    "7z${release}-extra.7z"
+    "7z${release}-src.7z"
+    "7zr.exe"
+  )
+} -NameMangle {
+  "7z${version}/${name}"
+}
