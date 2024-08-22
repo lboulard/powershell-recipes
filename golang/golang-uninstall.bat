04773ecea39a214b21bcd223737b4719a937b20f
Laurent Boulard <laurent.boulard@gmail.com>
2024-08-22 14:36:23 +0200

    Drop legacy golang folder, newer go folder is more capable

--- golang/golang-uninstall.bat
+++
@@ -1,7 +0,0 @@
-CALL "%~dp0golang-silent-install.bat" uninstall
-@IF ERRORLEVEL 1 GOTO :NagUser
-@GOTO :EOF
-@:NagUser
-@:: Pause if not interactive
-@ECHO %cmdcmdline% | FIND /i "%~0" >NUL
-@IF NOT ERRORLEVEL 1 PAUSE
