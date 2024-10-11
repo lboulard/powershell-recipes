b85fba28f8bf2459e070ad941c6aa05024cb5db3
Laurent Boulard <laurent.boulard@gmail.com>
2024-10-11 09:11:20 +0200

    In script for clink modules install, safely avoid final backslash
    Be careful and avoid backslash at path end.

--- clink_modules/clink-installscripts.bat
+++ clink_modules/clink-installscripts.bat
@@ -1,4 +1,4 @@
-@SETLOCAL
+@SETLOCAL ENABLEDELAYEDEXPANSION ENABLEEXTENSIONS
 @CHCP 65001 >NUL:
 @CD /D "%~dp0"
 @IF ERRORLEVEL 1 GOTO :exit
@@ -14,12 +14,16 @@
 @CALL :absolute "%~dp0..\clink"
 @PATH %RETVAL%;%PATH%
 
+:: script folder location, without final backslash
+@SET "HERE=%~dp0"
+@IF "%HERE:~-1%"=="\" SET "HERE=!HERE:~0,-1!"
+
 :: do not use dosbatch, our script is killed
 clink_x64.exe uninstallscripts --all
-clink_x64.exe installscripts "%~dp0\"
-clink_x64.exe installscripts "%~dp0clink-completions"
-clink_x64.exe installscripts "%~dp0clink-flex-prompt"
-clink_x64.exe installscripts "%~dp0clink-gizmos"
+clink_x64.exe installscripts "%HERE%"
+clink_x64.exe installscripts "%HERE%\clink-completions"
+clink_x64.exe installscripts "%HERE%\clink-flex-prompt"
+clink_x64.exe installscripts "%HERE%\clink-gizmos"
 
 
 @:: Pause if not interactive
@@ -32,4 +36,5 @@ clink_x64.exe installscripts "%~dp0clink-gizmos"
 
 @:absolute
 @SET "RETVAL=%~f1"
+@IF "%RETVAL:~-1%"=="\" SET "RETVAL=!RETVAL:~0,-1!"
 @GOTO :EOF
