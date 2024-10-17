b897d4a8148a5b5f0bdf09b099af9a560891a531
Laurent Boulard <laurent.boulard@gmail.com>
2024-10-17 10:07:15 +0200

    Download script for watchexec not longer need to fall back on 2.0.0 release

--- watchexec/latest.ps1
+++ watchexec/latest.ps1
@@ -10,18 +10,21 @@ Import-Module lboulard-Recipes
 
 Get-GitHubAssetsOfLatestRelease $project $tagPattern -FileSelection {
   @(
-  "watchexec-$version-x86_64-pc-windows-msvc.zip"
-  "watchexec-$version-x86_64-pc-windows-msvc.zip.sha256"
-  "watchexec-$version-x86_64-pc-windows-msvc.zip.b3"
-  ) | ForEach-Object { "$($_)#_broken_$($_)" }
+    "watchexec-$version-x86_64-pc-windows-msvc.zip"
+    "watchexec-$version-x86_64-pc-windows-msvc.zip.sha256"
+    "watchexec-$version-x86_64-pc-windows-msvc.zip.b3"
+  )
 }
 
-$goodVersion = "2.0.0"
-Write-Warning "`n::: Also using v$goodVersion, because v2.1.x builds broken on windows`n"
-$files = @(
-  "watchexec-$goodVersion-x86_64-pc-windows-msvc.zip"
-  "watchexec-$goodVersion-x86_64-pc-windows-msvc.zip.sha256"
-  "watchexec-$goodVersion-x86_64-pc-windows-msvc.zip.b3"
-) | ForEach-Object { "https://github.com/$project/releases/download/v$goodVersion/$_" }
+if ($false) {
+  # 2.2 fixed issue
+  $goodVersion = "2.0.0"
+  Write-Warning "`n::: Also using v$goodVersion, because v2.1.x builds broken on windows`n"
+  $files = @(
+    "watchexec-$goodVersion-x86_64-pc-windows-msvc.zip"
+    "watchexec-$goodVersion-x86_64-pc-windows-msvc.zip.sha256"
+    "watchexec-$goodVersion-x86_64-pc-windows-msvc.zip.b3"
+  ) | ForEach-Object { "https://github.com/$project/releases/download/v$goodVersion/$_" }
 
-Get-Url $files
+  Get-Url $files
+}
