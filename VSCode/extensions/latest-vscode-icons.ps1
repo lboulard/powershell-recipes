46c1392d6918151d2419a646457a13f7fd9fab92
Laurent Boulard <laurent.boulard@gmail.com>
2024-12-25 20:23:47 +0100

    Scripts to downloads VSCode extensions vscode-icons and vscode-powershell

---
+++ VSCode/extensions/latest-vscode-icons.ps1
@@ -0,0 +1,15 @@
+$ErrorActionPreference = "Stop"
+
+# https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_preference_variables?view=powershell-7.4#verbosepreference
+# $VerbosePreference = "Continue"
+
+$tagPattern = "(?<tag>v(?<version>\d+\.\d+(\.\d+)+))"
+$project = "vscode-icons/vscode-icons"
+
+Import-Module lboulard-Recipes
+
+Get-GitHubAssetsOfLatestRelease $project $tagPattern -FileSelection {
+  @(
+    "vscode-icons-${version}.vsix"
+  )
+}
---
+++ VSCode/extensions/latest-vscode-powershell.ps1
@@ -0,0 +1,27 @@
+$ErrorActionPreference = "Stop"
+
+# https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_preference_variables?view=powershell-7.4#verbosepreference
+# $VerbosePreference = "Continue"
+
+$tagPattern = "v(?<version>\d+\.\d+(\.\d+)+)(?:-preview)?"
+$project = "PowerShell/vscode-powershell"
+
+Import-Module lboulard-Recipes
+
+# filenames are not enough to find release files
+# use GitHub API
+
+$githubToken = Get-GitHubToken
+if (-not $githubToken) {
+  Write-Warning "GitHub token missing, failure or long delay can be expected"
+}
+
+$files = Find-GitHubReleaseFromAsset $project `
+  -TagPattern $tagPattern -AssetPattern "powershell-.*\.vsix" `
+  -Token $githubToken -PreRelease $true
+
+if ($files) {
+  Get-Url $files
+} else {
+  Write-Host "no files found"
+}
