d5d9789b8aaa87f17b848b619e4c815e5e8f68be
Laurent Boulard <laurent.boulard@gmail.com>
2025-02-23 12:30:05 +0100

    Deploy scripts for nightly build of WezTerm

---
+++ WezTerm/nightly-deploy.bat
@@ -0,0 +1,12 @@
+@SETLOCAL
+@CHCP 65001
+@where /q pwsh.exe
+@IF %ERRORLEVEL% EQU 0 (
+  pwsh.exe -noprofile "%~dpn0.ps1"
+) ELSE (
+  PowerShell.exe -noprofile "%~dpn0.ps1"
+)
+@SET ERR=%ERRORLEVEL%
+@ECHO %cmdcmdline% | FIND /i "%~0" >NUL
+@IF NOT ERRORLEVEL 1 PAUSE
+@ENDLOCAL&EXIT /B %ERR%
---
+++ WezTerm/nightly-deploy.ps1
@@ -0,0 +1,58 @@
+﻿$ErrorActionPreference = "Stop"
+
+$root = $PSScriptRoot
+
+$filenameRegex = '^Wezterm-windows-\d{8}-\d+-[0-9a-f]+$'
+
+$folder = Join-Path $root "nightly"
+$archive = Join-Path $folder "WezTerm-windows-nightly.zip"
+
+$lbPrograms = $Env:LBPROGRAMS
+$prefix = $lbPrograms
+$dest = Join-Path $prefix "Apps"
+
+if (-not $prefix) {
+  throw "prefix not defined. Is LBPROGRAMS environment variable defined?"
+}
+
+if (-not (Test-Path $dest -PathType Container)) {
+  throw "${prefix}: directory not found"
+}
+
+## Breaks on multifile archive
+# Expand-Archive -DestinationPath $dest -LiteralPath $archive -Force
+
+$currentDir = Get-Location
+try {
+  Set-Location $dest
+  Write-Host " ✓ " -NoNewline -ForegroundColor Green
+  Write-Host "${archive}: expand to ${dest}"
+  & jar xvf $archive
+  if ($LASTEXITCODE) {
+    throw "${archive}: execution failed with exit code ${LASTEXITCODE}"
+  }
+} finally {
+  Set-Location $currentDir
+}
+
+# find last version
+$lastVersion = Get-ChildItem $dest -Directory | Select-Object -ExpandProperty Name | Where-Object {
+  $_ -match $filenameRegex
+} | Sort-Object -Descending | Select-Object -First 1
+
+if (-not $lastVersion) {
+  [Console]::Error.WriteLine($json)
+  throw "no folder versions found"
+}
+
+# create symbolic link
+
+$currentDir = Get-Location
+try {
+  Set-Location $dest | Out-Null
+  Write-Host " ✓ " -NoNewline -ForegroundColor Green
+  Write-Host "Symbolic-Link: ./weterm → ./$lastVersion"
+  New-Item -ItemType SymbolicLink -Path "./wezterm" -Target "./$lastVersion" -Force | Out-Null
+} finally {
+  Set-Location $currentDir
+}
