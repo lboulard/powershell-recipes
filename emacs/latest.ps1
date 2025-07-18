1f949d37560348749d0732709c1ad70efc563f5b
Laurent Boulard <laurent.boulard@gmail.com>
2025-07-18 14:45:07 +0200

    Scripts for latest emacs needs more robustness

--- emacs/latest.ps1
+++ emacs/latest.ps1
@@ -2,28 +2,35 @@ $ErrorActionPreference = "Stop"
 
 # https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_preference_variables?view=powershell-7.4#verbosepreference
 # $VerbosePreference = "Continue"
+# $DebugPreference = "Continue"
 
 # Find latest "release-.*" folder
 
+# $IndexURL = "https://ftp.gnu.org/gnu/emacs/windows/"
 $IndexURL = "https://mirror.ibcp.fr/pub/gnu/emacs/windows/"
 $versionPattern = "^emacs-(?<version>\d+\.\d+(_\d)?)(\.zip|-sha256.+\.txt)(\.sig)?$"
 
 Import-Module lboulard-Recipes
 
+Write-Debug "Reading $IndexURL"
 $links = (Invoke-HtmlRequest $IndexURL).Links
+if (!$links) {
+  throw "no links found"
+}
 
 $url = [System.Uri]$IndexURL
 
 $files = $links.href | ForEach-Object {
-  if ($_ -match "^emacs-(?<major>\d+)") {
+  if ($_ -match "^(?<folder>emacs-(?<major>\d+))") {
     [PSCustomObject]@{
-      Folder = $_
-      Major  = $Matches.major
+      Folder = $Matches.folder + '/'
+      Major  = [int]$Matches.major
     }
   }
 } | Sort-Object -Descending -Property Major | Select-Object -First 2 | ForEach-Object {
   $releaseURL = New-Object System.Uri -ArgumentList $url, $_.Folder
-  $releases = (Invoke-WebRequest $releaseURL -UseBasicParsing).Links
+  Write-Debug "Reading $releaseURL"
+  $releases = (Invoke-WebRequest -Uri $releaseURL -UseBasicParsing).Links
   $releases.href | ForEach-Object {
     if ($_ -match $versionPattern) {
       # Transformation for sorting release
