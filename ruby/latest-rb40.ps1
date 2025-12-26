a62d3aa60095a28ceaf391078af945cb9850aff0
Laurent Boulard <laurent.boulard@gmail.com>
2025-12-26 17:35:47 +0100

    Prepare for Ruby 4.0

---
+++ ruby/latest-rb40.ps1
@@ -0,0 +1,48 @@
+# PowerShell 5 and 7
+# find Ruby 4.0 installer using GitHub API only walking releases
+
+$ErrorActionPreference = "Stop"
+
+# https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_preference_variables?view=powershell-7.4#verbosepreference
+# $VerbosePreference = "Continue"
+
+$branch = "4.0"
+
+$tagPattern = "RubyInstaller-(?<version>$([regex]::escape($branch))\.\d+)(\-\d+)?$"
+$wanted = "^rubyinstaller-$([regex]::escape($branch))\.\d+.+?-x64\.(exe|7z)(\.asc)?$"
+$project = "oneclick/rubyinstaller2"
+
+$nameMangle = {
+  # manipulate $name, $version and $tag are accessible from tag pattern parsing
+  Write-Verbose "`$name='$name', `$version='$version'"
+  "${branch}/${name}"
+}
+
+Import-Module lboulard-Recipes
+
+# filenames are not enough to find release files
+# use GitHub API
+
+$githubToken = Get-GitHubToken
+if (-not $githubToken) {
+  Write-Warning "GitHub token missing, failure or long delay can be expected"
+}
+
+# parse all request until we find a release that match wanted files
+
+$files = Find-GitHubReleaseFromAsset $project $tagPattern $wanted -Token $githubToken -ReleaseScript {
+  param($release)
+  Write-Host "Release tag: $($release.tag_name)"
+} -NameMangle $nameMangle
+
+if (-not $files) {
+  Write-Error "no files found"
+  exit 1
+}
+
+$headers = @{
+  'Accept'        = 'application/octet-stream'
+  'Authorization' = 'token ' + $githubToken
+}
+
+Get-Url $files -Headers $headers -ProjectName ruby4.0
